866bcc76f02f8c7008ed6152abff69c3a72bd65c
FelpGV <f.gil@utp.edu.co>
2024-03-14 13:46:07 -0500

    Test Finca

--- vivero/viveroApp/tests/fincaTest.py
+++ vivero/viveroApp/tests/fincaTest.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
-from ..models import Finca, Productor
+from ..models import Finca, Productor, Vivero
 from ..views.fincaView import *
+from django.core.exceptions import ValidationError
 
 
 class FincaTestCase(TestCase):
@@ -12,14 +13,48 @@ class FincaTestCase(TestCase):
             telefono='333',
             correo='a.b@test.com'
         )
-        Finca.objects.create(
+        self.finca = Finca.objects.create(
             productor=self.productor,
             numeroCatastro='001',
             municipioUbicacion='Pereira'
         )
+        self.vivero1 = Vivero.objects.create(
+            finca=self.finca,
+            codigo='V1',
+            tipoCultivo='Cultivo1'
+        )
+        self.vivero2 = Vivero.objects.create(
+            finca=self.finca,
+            codigo='V2',
+            tipoCultivo='Cultivo2'
+        )
 
-    def test_finca(self):
+    def testFinca(self):
         finca = Finca.objects.get(numeroCatastro='001')
         self.assertEqual(finca.productor, self.productor)
         self.assertEqual(finca.numeroCatastro, '001')
         self.assertEqual(finca.municipioUbicacion, 'Pereira')
+
+    def testCamposObligatorios(self):
+        with self.assertRaises(ValidationError):
+            # No tiene asignado un municipio
+            registrarFinca(1, "002", "")
+
+    def testRegistrarFinca(self):
+        finca = registrarFinca(1, '003', 'Dosquebradas')
+        self.assertEqual(finca.productor, self.productor)
+        self.assertEqual(finca.numeroCatastro, '003')
+        self.assertEqual(finca.municipioUbicacion, 'Dosquebradas')
+
+    def testObtenerViverosFinca(self):
+        finca = Finca.objects.get(numeroCatastro='001')
+        viveros = obtenerViverosFinca(finca.id)
+        self.assertIn(self.vivero1, viveros)
+        self.assertIn(self.vivero2, viveros)
+        self.assertEqual(viveros.count(), 2)
+
+    def testEliminarFinca(self):
+        idFinca = self.finca.id
+        eliminarFinca(idFinca)
+        # assertTrue(expr)/assertFalse(expr) .exist devuelve verdadero o false
+        self.assertFalse(Finca.objects.filter(id=idFinca).exists())
--- vivero/viveroApp/views/fincaView.py
+++ vivero/viveroApp/views/fincaView.py
@@ -2,14 +2,25 @@ from ..models.finca import Finca
 from ..models.productor import Productor
 
 
-def register_finca(productorId, numeroCatastro, municipioUbicacion):
+def registrarFinca(productorId, numeroCatastro, municipioUbicacion):
     productor = Productor.objects.get(id=productorId)
 
     finca = Finca(
-        productor=productorId,
-        numero_catastro=numeroCatastro,
+        productor=productor,
+        numeroCatastro=numeroCatastro,
         municipioUbicacion=municipioUbicacion
     )
     finca.full_clean()
     finca.save()
+    return finca
+
+
+def obtenerViverosFinca(idFinca):
+    finca = Finca.objects.get(id=idFinca)
+    return finca.vivero_set.all()
+
+
+def eliminarFinca(idFinca):
+    finca = Finca.objects.get(id=idFinca)
+    finca.delete()
     return finca
